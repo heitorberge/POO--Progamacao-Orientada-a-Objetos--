a4154f95d3402c473ffff370f0cb259605f08071
Heitor Berg <heitorberg114@gmail.com>
2025-02-22 10:32:13 -0300

    Delete Poo(Progamação Orientada a Objeto) directory

--- Poo(Progamação Orientada a Objeto)/Aula04/ProjetoCaneta03/src/main/java/cursoemvideo/projetocaneta03/Aula04.java
+++
@@ -1,9 +0,0 @@
-package cursoemvideo.projetocaneta03;
-public class Aula04 {
-    public static void main(String[] args) {
-        Caneta c1 = new Caneta("NIC", 0.4f , "Amarelo");
-        c1.status();
-        Caneta c2 = new Caneta("KKK", 1.5f, "Laranja");
-        c2.status();
-    }
-}
